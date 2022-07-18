a692192fa2ff9e28fa5bf905cb3910e0c80923f0
Maxim Movchan <m.movchan@spb.mcart.ru>
2022-07-18 23:45:13 +0300

    first commit

--- android/app/src/main/kotlin/com/example/wuskan/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.wuskan
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ lib/ui/main/ui/main_screen.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive/hive.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:wuskan/gen/assets.gen.dart';
+import 'package:wuskan/models/user/user_model.dart';
+import 'package:wuskan/ui/game/ui/game_screen.dart';
+import 'package:wuskan/ui/home/uikit/sum_container.dart';
+import 'package:wuskan/uikit/main_button.dart';
+import 'package:wuskan/utils/color_palette/colors.dart';
+import 'package:wuskan/utils/typography/apptypography.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedSum = 250;
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        child: Scaffold(
+          backgroundColor: AppColors.darkBlue,
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.business),
+                label: 'Business',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.school),
+                label: 'School',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.amber[800],
+            onTap: _onItemTapped,
+          ),
+          floatingActionButton: MainButton(
+              onTap: () => Navigator.push(
+                    context,
+                    PageTransition<Widget>(
+                      child: GameScreen(
+                        bet: selectedSum,
+                      ),
+                      type: PageTransitionType.rightToLeft,
+                    ),
+                  ),
+              label: 'Start the game'),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          body: SafeArea(
+            minimum: EdgeInsets.all(16.w),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      '${Hive.box<UserModel>('user').values.first.balance!}',
+                      style: AppTypography.main.copyWith(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20.w,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 5.w,
+                      ),
+                      child: Image.asset(
+                        Assets.images.coin.path,
+                        filterQuality: FilterQuality.high,
+                        width: 27.w,
+                        height: 27.h,
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 30.h, bottom: 8.h),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Choose your bet',
+                        style: AppTypography.main.copyWith(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28.w,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: 16.h),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 343.w,
+                        child: Text(
+                          'If your amount is less than 1000, then the next day it will become 1000 again',
+                          style: AppTypography.main.copyWith(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16.w,
+                              color: AppColors.gray.withOpacity(0.6)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+               Expanded(child:  ListView(
+                 children: [
+                   SumContainer(
+                     isSelected: selectedSum == 10,
+                     label: 'Training',
+                     level: 1,
+                     price: 10,
+                     onPressed: () => setState(() {
+                       selectedSum = 10;
+                     }),
+                   ),
+                   SumContainer(
+                     isSelected: selectedSum == 50,
+                     label: 'Easy',
+                     level: 2,
+                     price: 50,
+                     onPressed: () => setState(() {
+                       selectedSum = 50;
+                     }),
+                   ),
+                   SumContainer(
+                     isSelected: selectedSum == 250,
+                     label: 'Medium',
+                     level: 3,
+                     price: 250,
+                     onPressed: () => setState(() {
+                       selectedSum = 250;
+                     }),
+                   ),
+                   SumContainer(
+                     isSelected: selectedSum == 500,
+                     label: 'Hard',
+                     level: 4,
+                     price: 500,
+                     onPressed: () => setState(() {
+                       selectedSum = 500;
+                     }),
+                   ),
+                   SumContainer(
+                     isSelected: selectedSum == 1000,
+                     label: 'Insane',
+                     level: 5,
+                     price: 1000,
+                     onPressed: () => setState(() {
+                       selectedSum = 1000;
+                     }),
+                   ),
+                   SizedBox(height: 90.h,),
+                 ],
+               ),)
+              ],
+            ),
+          ),
+        ),
+        onWillPop: () async => false);
+  }
+}
